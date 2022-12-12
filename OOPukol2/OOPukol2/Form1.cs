66fb7578e3a298e21a029d93dd28713933cf8efd
Mrnight020 <97515527+Mrnight020@users.noreply.github.com>
2022-12-12 18:32:29 +0100

    Add files via upload

---
+++ OOPukol2/OOPukol2/Form1.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace OOPukol2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        NakladniAuto noveauto;
+        private void button1_Click(object sender, EventArgs e)
+        {
+            noveauto = new NakladniAuto(textBox1.Text,(short) numericUpDown3.Value);
+            panel1.Enabled = true;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            noveauto.NalozNaklad((short)numericUpDown1.Value);
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            noveauto.VylozNaklad((short)numericUpDown2.Value);
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show(noveauto.ToString());
+        }
+    }
+}
---
+++ OOPukol2/OOPukol2/NakladniAuto.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace OOPukol2
+{
+    internal class NakladniAuto
+    {
+
+        private string spz;
+        private short nosnost; 
+
+        private short hmotnostnakladu;
+        public short HmotnostNakladu { get => hmotnostnakladu; }
+
+        public NakladniAuto(string spz, short nosnost)
+        {
+            this.spz = spz;
+            this.nosnost = nosnost;
+        }
+
+        public void NalozNaklad(short naklad)
+        {
+            hmotnostnakladu += naklad;
+            if (hmotnostnakladu <= nosnost)
+            {
+                MessageBox.Show("bylo nalozeno: " + hmotnostnakladu + " tun");
+            }
+            else
+            {
+                MessageBox.Show("nebylo nalozeno: " + (hmotnostnakladu - nosnost) + " tun");
+                hmotnostnakladu = nosnost;
+            }
+        }
+
+        public void VylozNaklad(short vylozit)
+        {
+            if (vylozit > hmotnostnakladu)
+            {
+                MessageBox.Show("Bylo maximalne vylozeno " + hmotnostnakladu + " tun chybi vylozit " + (vylozit - hmotnostnakladu) + " tun");
+                hmotnostnakladu = 0;
+            }
+            else
+            {
+                MessageBox.Show("Bylo vylozeno " + vylozit + " tun");
+                hmotnostnakladu -= vylozit;
+            }
+        }
+
+        public override string ToString()
+        {
+            return String.Format("Nákladní auto {0} má nosnost {1} tun a má naloženo {2} tun", spz, nosnost, hmotnostnakladu);
+        }
+    }
+}
